7ef79dab8ca3119a3626a5ba17bbbe7d27d50c6c
Hayate_T <hayatesato1@gmail.com>
2023-03-04 17:53:40 +0900

    todosテーブルのカラム実装

--- db/migrate/20230304084158_create_todos.rb
+++ db/migrate/20230304084158_create_todos.rb
@@ -1,7 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-
+      t.references :user, foreign_key: true
+      t.string :todos
       t.timestamps
     end
   end
