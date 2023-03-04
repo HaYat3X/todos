1ed940ac9750e91605a33dc5ac61de1e7e1e7734
Hayate_T <hayatesato1@gmail.com>
2023-03-04 17:57:38 +0900

    todos新規作成メソッドの導入

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,22 @@
 class TodosController < ApplicationController
   # ! タスクを新規投稿するメソッド
   def new
+    @todo = Todo.new
   end
 
   # ! タスクを新規投稿するメソッド
   def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to "/"
+    else
+      redirect_to "/"
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:todos)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_082734) do
+ActiveRecord::Schema.define(version: 2023_03_04_084158) do
+
+  create_table "todos", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "todos"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_03_04_082734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "todos", "users"
 end
