fee1c5941094dc5a600f5ae1cfbf95ab6280c77d
Hayate_T <hayatesato1@gmail.com>
2023-03-05 17:17:03 +0900

    jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,11 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 require("jquery")
 
+// * フラッシュメッセージを2秒後に消す
+$(function() {
+    setTimeout("$('.alert').fadeOut('slow')", 1000);
+});
+
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
